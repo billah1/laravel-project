0409999e4ef52e737e2163771a4381e7ef582f3b
billah1 <billah9578@gmail.com>
2023-01-08 12:59:36 +0600

    soft deletes

--- database/migrations/2022_12_25_062529_create_categories_table.php
+++ database/migrations/2022_12_25_062529_create_categories_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('slug');
             $table->boolean('is_active')->default(1);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
